f9885ce429355687864ea11119e183abc5c19fe1
mustha <mustha@musthas-MacBook-Air.local>
2026-01-11 20:59:07 +0530

    commit3

--- music-frontend/src/services/api.js
+++ music-frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "http://localhost:5005/api"
-   baseURL: import.meta.env.VITE_API_URL
+  baseURL: "http://localhost:5005/api"
+  //  baseURL: import.meta.env.VITE_API_URL
 
 });
 
